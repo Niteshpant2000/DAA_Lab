8c306afa2447574a1339eb2768c5bca3098377e7
Niteshpant2000 <nitesh.2000pant@gmail.com>
2021-08-01 15:17:37 +0530

    jump search

--- week1/jump.py
+++ week1/jump.py
@@ -1,35 +1,25 @@
 import math
 t=int(input())
-for i in range(1,t+1):
+for i in range(0,t):
     n=int(input())
     arr=list(map(int,input().split(" ")))
     key=int(input())
-    count=0
+    jump=int(math.sqrt(n))
+    prev=0
+    step=jump
+    comp=0
     flag=0
-    start=0
-    end=n-1
-    for i in range(0,int(math.log(n,2))):
-        if(arr[2**i]<key):
-            start=2**i
-            count+=1
-        if(arr[2**i]>key):
-            end=2**i
-            count+=1
-            break
-        if (arr[2**i]==key):
-            count+=1
-            flag=1
+    while(arr[int(min(n,step))-1] <key):
+        comp+=1
+        prev=step
+        step+=jump
+    for i in range(prev,step+1):
+        comp+=1
+        if(arr[i]==key):
+            print(f"Present {comp}")
             break
     if(flag==0):
-        for i in range(start+1,end+1):
-            if(arr[i]==key):
-                count+=1
-                flag=1
-                break
-            count+=1
-    print(start,end)
-    if(flag==0):
-        print(f"Not Present {count}")
-    else:
-        print(f"Present {count}")
+        print(f"not present {comp}")
+    
+
 
